28470300fc4d663051bbf9a4e670680253280502
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-01-13 11:56:01 +0900

    memo

--- src/components/molecules/SearchInput.jsx
+++ src/components/molecules/SearchInput.jsx
@@ -1,9 +1,11 @@
+import React, { memo } from "react";
 import styled from "styled-components";
 
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { Input } from "../atoms/input/Input";
 
-export const SearchInput = () => {
+export const SearchInput = memo(() => {
+  console.log("SearchInput");
   return (
     <div>
       <SContainer>
@@ -14,7 +16,7 @@ export const SearchInput = () => {
       </SContainer>
     </div>
   );
-};
+});
 
 const SContainer = styled.div`
   display: flex;
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,5 +1,7 @@
+import React, { useContext } from "react";
 import styled from "styled-components";
-
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { UserContext } from "../providers/UserProviders";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -18,10 +20,14 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
   return (
     <SContainer>
       <h2>Users</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>Switch</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={users.id} user={user} />
